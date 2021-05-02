3fa6bdd5d0df5074b07b42a95fb453a5994fef1c
Dhruvesh <dhruvesh.baria@actonate.com>
2021-05-02 16:19:58 +0530

    first commit

---
+++ layouts/componnents/MainShell/team.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+ const Teams = () => {
+    return (
+        <main className="flex-1 relative overflow-y-auto focus:outline-none">
+        <div className="py-6">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            <h1 className="text-2xl font-semibold text-gray-900">Teams</h1>
+          </div>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            {/* Replace with your content */}
+            <div className="py-4">
+              <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+            </div>
+            {/* /End replace */}
+          </div>
+        </div>
+      </main>
+    )
+}
+
+export default Teams;
